1ec12a4468bf1c7541aea880dbc849b22466ea92
Stefano <stefano.pettini@disc.it>
2021-03-04 06:52:24 +0100

    Variazione alla documentazione

--- src/log.php
+++ src/log.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * scrive come al primo posto la data/ora attuale, poi il loglevel e poi il messaggio
+ * scrive un file con al primo posto la data/ora attuale, poi il loglevel e poi il messaggio
  * es.
  * 03/03/2021 16:22 DEBUG messaggio di debug
  * 03/03/2021 16:23 DEBUG messaggio di debug ulteiroire
@@ -18,14 +18,26 @@ function writeOnFile($loglevel, $message){
     fclose($myfile);
 }
 
+/**
+ * Scrive il log di livello INFO
+ * @param $messagio il messaggio
+ */
 function logInfo($messagio){
     writeOnFile("INFO", $messagio);
 }
 
+/**
+ * Scrive il log di livello DEBUG
+ * @param $messagio il messaggio
+ */
 function logDebug($messagio){
     writeOnFile("DEBUG", $messagio);
 }
 
+/**
+ * Scrive il log di livello ERROR
+ * @param $messagio il messaggio
+ */
 function logError($messagio){
     writeOnFile("ERROR", $messagio);
 }
